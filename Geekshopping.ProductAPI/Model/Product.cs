c44c7a18ec71b5ce655579a634005cde91abf795
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-12 17:33:03 -0300

    Criando entidade Produtc.

--- Geekshopping.ProductAPI/Model/Product.cs
+++ Geekshopping.ProductAPI/Model/Product.cs
@@ -1,4 +1,30 @@
-﻿namespace Geekshopping.ProductAPI.Model {
-    public class Product {
+﻿using Geekshopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Geekshopping.ProductAPI.Model {
+    [Table("Product")]
+    public class Product : BaseEntity {
+        [Column("Name")]
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+        
+        [Column("Price")]
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+
+        [Column("Description")]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Column("Category_Name")]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [Column("Image_URL")]
+        [StringLength(255)]
+        public string ImageURL { get; set; }
     }
 }
