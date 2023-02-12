f822848ece46e7c86b346282dddb501ae5eabf59
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-12 18:03:34 -0300

    Corrigindo algumas informações do projeto.

--- Geekshopping.ProductAPI/Program.cs
+++ Geekshopping.ProductAPI/Program.cs
@@ -1,5 +1,6 @@
 using Geekshopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,12 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connecti
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Geek Shopping",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
