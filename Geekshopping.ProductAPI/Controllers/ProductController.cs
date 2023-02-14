cefc0a64e698f592801b74b437b738cd572a052f
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-13 22:49:46 -0300

    Atualizando o bootstrap, implementando o fontawesome e inserindo logo e faveicon do site.

--- Geekshopping.ProductAPI/Controllers/ProductController.cs
+++ Geekshopping.ProductAPI/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO) {
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO productVO) {
             if (productVO == null) {
                 return BadRequest();
             }
@@ -41,7 +41,7 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO) {
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO productVO) {
             if (productVO == null) {
                 return BadRequest();
             }
