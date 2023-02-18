9e618d1ad35208d0322e84a84d916202f07089f8
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-18 15:29:49 -0300

    Incluido as autorizações na solução para acessar os dados.

--- Geekshopping.ProductAPI/Controllers/ProductController.cs
+++ Geekshopping.ProductAPI/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using GeekShopping.ProductAPI.Data.ValueObjects;
 using GeekShopping.ProductAPI.Repository;
+using GeekShopping.ProductAPI.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.ProductAPI.Controllers {
@@ -13,12 +15,14 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll() {
             var products = await _repository.FindAll();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> FindById(long id) {
             var product = await _repository.FindById(id);
             if (product.Id <= 0) {
@@ -30,6 +34,7 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO productVO) {
             if (productVO == null) {
                 return BadRequest();
@@ -41,6 +46,7 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpPut]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO productVO) {
             if (productVO == null) {
                 return BadRequest();
@@ -52,6 +58,7 @@ namespace GeekShopping.ProductAPI.Controllers {
         }
 
         [HttpDelete("{id}")]
+        [Authorize(Roles = Role.Admin)]
         public async Task<ActionResult> Delete(long id) {
             var status = await _repository.Delete(id);
             if (!status) {
