3bc0365ac6b7e9a78a8f9190419f1ce88b6345f0
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-18 12:33:32 -0300

    Criando e definindo a configuração da identity.

---
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace GeekShopping.IdentityServer.Configuration {
+    public static class IdentityConfiguration {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> identityResources => new List<IdentityResource> {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<ApiScope> apiScopes => new List<ApiScope> {
+            new ApiScope("geek_shopping", "GeekShopping Server"),
+            new ApiScope("read", "Read data."),
+            new ApiScope("write", "Write data."),
+            new ApiScope("delete", "Delete data.")
+        };
+
+        public static IEnumerable<Client> clients => new List<Client> {
+            new Client {
+                ClientId = "client",
+                ClientSecrets = {
+                    new Secret("my_super_secret".Sha256())
+                },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {
+                    "read",
+                    "write",
+                    "profile"
+                }
+            }
+        };
+    }
+}
